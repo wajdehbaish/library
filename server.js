65b81b1e5649b59267d838c49eb4867bdadb1ae6
wajdehbaish <wajdehbaish@gmail.com>
2021-11-15 16:14:35 +0200

    done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const booksRouter = require("./routes/books.route")
+const booksRouter = require("./routes/books.route");
 
 app.use(express.json());
 
@@ -10,15 +10,12 @@ app.use(cors());
 
 app.use("/", booksRouter);
 
-
-
 mongoose.connect(
-    "mongodb+srv://wajde:123456789@cluster0.lg6xi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-      console.log("Connected to DB");
-    }
-  );
-  
-  app.listen(process.env.PORT||4000);
-  
+  "mongodb+srv://wajde:wajde123@cluster0.lg6xi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to DB");
+  }
+);
+
+app.listen(process.env.PORT || 4000);
